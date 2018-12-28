b86f93634ba626582452e85645d72f6f65761550
Brian Bycynski <high5bri@hotmail.com>
2018-12-28 08:59:33 -0500

    just finished first hour of tutorial

--- src/App.js
+++ src/App.js
@@ -5,10 +5,35 @@ import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 class App extends Component {
+  state = {
+    recipes: recipes,
+    url:
+      'https://www.food2fork.com/api/search?key=8cdf1793f844fc29f814093719d93807&'
+  };
+
+  // async getRecipes() {
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+
+  //     this.setState({
+  //       recipes: jsonData.recipes
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   this.getRecipes();
+  // }
+
   render() {
+    // console.log(this.state.recipes);
+
     return (
       <React.Fragment>
-        <RecipeList />
+        <RecipeList recipes={this.state.recipes} />
         <RecipeDetails />
       </React.Fragment>
     );
